6883223e72d7cdc2209ca4c7d25940a5ad061e00
nickglovdev <nickglovdev@gmail.com>
2020-03-05 14:26:02 -0600

    Added the folder structure and got the text to automate. Working now on flex box.

---
+++ scripts/dad/Dad.js
@@ -0,0 +1,9 @@
+export const Dad = (dadObjects) => {
+    return`
+        <ul class="dadJokeContainer">
+            <li class="joke__list"><h4>${dadObjects.id}</h4></li>
+            <li class="joke__list">${dadObjects.question}</li>
+            <li class="joke__list">${dadObjects.answer}</li>
+        </ul>
+    `
+}
