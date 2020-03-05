69467653a0ddf2cfa86b2bc6caea9089bd5bec1e
nickglovdev <nickglovdev@gmail.com>
2020-03-05 15:00:23 -0600

    Added some more CSS changes to make the flex box work better.

--- scripts/dad/Dad.js
+++ scripts/dad/Dad.js
@@ -3,7 +3,7 @@ export const Dad = (dadObjects) => {
         <ul class="dadJokeContainer">
             <li class="joke__list"><h4>${dadObjects.id}</h4></li>
             <li class="joke__list">${dadObjects.question}</li>
-            <li class="joke__list">${dadObjects.answer}</li>
+            <li class="joke__answer joke__list">${dadObjects.answer}</li>
         </ul>
     `
 }
