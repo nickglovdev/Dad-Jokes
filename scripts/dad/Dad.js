ce58f8d6233b2a54c1c4ed44209d82b08ef662f3
nickglovdev <nickglovdev@gmail.com>
2020-03-05 15:49:32 -0600

    Was able to add a dialog button for the punch lines.

--- scripts/dad/Dad.js
+++ scripts/dad/Dad.js
@@ -1,9 +1,15 @@
 export const Dad = (dadObjects) => {
     return`
         <ul class="dadJokeContainer">
-            <li class="joke__list"><h4>${dadObjects.id}</h4></li>
+            <li class="joke__list"><h4>Joke Number: ${dadObjects.id}</h4></li>
             <li class="joke__list">${dadObjects.question}</li>
-            <li class="joke__answer joke__list">${dadObjects.answer}</li>
+            <li>
+                <button class="jokeButton" id="button--${dadObjects.id}">Answer</button>
+                <dialog class="dialog--joke" id="details--${dadObjects.id}">
+                    <div>${dadObjects.answer} </div>
+                    <button class="button--close">Close Dialog</button>
+                </dialog>
+            </li>
         </ul>
     `
 }
